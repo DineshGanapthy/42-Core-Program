bbb24e3251ffd886e62e6d3ee62ce9629068b88e
Dinesh Ganapathy <dinesh.0749@gmail.com>
2026-03-13 15:21:22 +0800

    With comments

---
+++ push_swap_trial 2/commands/rotate.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dganapat <dganapat@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 11:31:30 by dganapat          #+#    #+#             */
+/*   Updated: 2026/03/11 14:31:47 by dganapat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+static void rotate(t_stack_node **stack) // A fuction to rotate the stacks top node to the bottom and move
+										// everthing up by one. 
+{
+	t_stack_node	*last_node; // A t_stack_node pointer to store the pointer to the last node. 
+
+	if (!*stack || !(*stack)->next) // To check if the stack is empty or if there is one node. 
+		return ;
+	last_node = find_last(*stack); // find last node in stack and assign it to last_node.
+	last_node->next = *stack; // Pointing the last nodes next pointer to the current head pointer. Making 5's next point to 1.
+	*stack = (*stack)->next; // Assigning the head node as the second node in the stack.
+	(*stack)->prev = NULL; // Assigning the new head's previous pointer to NULL. (detaching it from the previous top)
+	last_node->next->prev = last_node; // Saying the old head (1) which is now the new last node's(1) previous pointer 
+										//is now pointing to the old last_node(5). 1's prev is 5.
+	last_node->next->next = NULL; // Now we are assigning the new last nodes's next pointer to NuLL to properly NULL terminate the stack. 
+}
+
+void	ra(t_stack_node **a, bool print) // rotate a first to last, second to first, last to sencond last,
+{
+	rotate(a);
+	if (!print)
+		ft_printf("ra\n");
+}
+
+void	rb(t_stack_node **b, bool print) // rorate b, evertything up by one.
+{
+	rotate(b);
+	if (!print)
+		ft_printf("rb\n");
+}
+
+void	rr(t_stack_node **a, t_stack_node **b, bool print) // rotate a and b
+{
+	rotate(a);
+	rotate(b);
+	if (!print)
+		ft_printf("rr\n");
+}
+
--- push_swap_trial 2/rotate.c
+++ push_swap_trial 2/rotate.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list.h                                          :+:      :+:    :+:   */
+/*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dganapat <dganapat@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/10 16:07:34 by dganapat          #+#    #+#             */
-/*   Updated: 2026/03/10 16:19:13 by dganapat         ###   ########.fr       */
+/*   Created: 2026/03/04 13:31:12 by dganapat          #+#    #+#             */
+/*   Updated: 2026/03/04 13:35:13 by dganapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_LIST_H
-# define FT_LIST_H
-
-//We create a struct type with the two variables required "Data" and "Next."
-
-typedef		struct	s_list
+void	rr(t_stack_node **a, t_stack_node **b, bool print)
 {
-	struct	s_list	*next; // It contains a struct s-list pointer to the next node.
-	void			*data; // The pointer to the data it contains.
-	
-}					t_list; // We the call this stucture t_list. 
+	rotate(a);
+	rotate(b);
+	if (!print)
+		ft_print("rr\n");
+}
 
-t_list	*ft_create_elem(void *data);
-
-#endif
+void	rotate_both(t_stack_node **a,
+					t_stack_node **b
+					t_stack_node *cheapest_node)
+{
+	while (*b != cheapest_node->target_node && *a != cheapest_node)
+		rr(a, b, false);
+	current_index(*a);
+	current_index(*b);
+}
