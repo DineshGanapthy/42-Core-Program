dad8dabe9a0aa12099af03e999cddc35e2e7eac0
Dinesh Ganapathy <dganapat@u91z08s01.42kl.edu.my>
2026-04-07 14:38:23 +0800

    testing One

---
+++ push_swap/push_swap_1/sort_three.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dganapat <dganapat@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/03 15:49:29 by dganapat          #+#    #+#             */
+/*   Updated: 2026/04/07 11:39:52 by dganapat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_three(t_stack_node	**a)
+{
+	t_stack_node	*biggest_node;
+
+	biggest_node = find_max(*a);
+	if (biggest_node == *a)
+		ra(a, false);
+	else if ((*a)->next == biggest_node)
+		rra(a, false);
+	if ((*a)->nbr > (*a)->next->nbr)
+		sa(a, false);
+}
+/*
+static void	min_on_top(t_stack_node **a)
+{
+	while ((*a)->nbr != find_min(*a)->nbr)
+	{
+		if (find_min(*a)->above_median)
+			ra(a, false);
+		else
+			rra(a, false);
+	}
+}
+*/
---
+++ push_swap/push_swap_1/swap.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dganapat <dganapat@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 11:49:31 by dganapat          #+#    #+#             */
+/*   Updated: 2026/04/07 11:09:08 by dganapat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	swap(t_stack_node **head)
+{
+	if (!*head || !(*head)->next)
+		return ;
+	*head = (*head)->next;
+	(*head)->previous->previous = *head;
+	(*head)->previous->next = (*head)->next;
+	if ((*head)->next)
+		(*head)->next->previous = (*head)->previous;
+	(*head)->previous = NULL;
+}
+
+void	sa(t_stack_node **a, bool print)
+{
+	swap(a);
+	if (!print)
+		write(1, "sa\n", 3);
+}
+
+void	sb(t_stack_node **b, bool print)
+{
+	swap(b);
+	if (!print)
+		write(1, "sb\n", 3);
+}
+
+void	ss(t_stack_node **a, t_stack_node **b, bool print)
+{
+	swap(a);
+	swap(b);
+	if (!print)
+		write(1, "ss\n", 3);
+}
